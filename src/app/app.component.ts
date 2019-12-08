6cf2cd0078efbdcfb9983e2731379db13cfa0119
moamen ahmed lotfy mahmoud <eng_moemenahmed2010@yahoo.com>
2019-12-08 11:27:25 +0200

    Angulat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyAngularProject';
+  name='Moamen Ahmed';
+  age=31
 }
+
