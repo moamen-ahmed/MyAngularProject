0cc5efc7351e22fd7148c624b50b3f072628771b
moamen ahmed lotfy mahmoud <eng_moemenahmed2010@yahoo.com>
2019-12-15 11:03:10 +0200

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ colors=['Red','Green','Purple','Yellow','White','Black'];
+
   name='Moamen Ahmed';
   age=31
+  source='https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1559151354/fczri2sftwzu4afgol9o.jpg';
+  myFunction(){
+    alert('Salamo Alaikom');
+  }
+  
+
+  onChange()
+  {
+    console.log('Color Changed !');
+  }
+
 }
+
+
+
 
